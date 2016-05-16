ace641f01965d53668b19e108464cd0106d77f3d
Tom Mahony <tsmahony@gmail.com>
2016-05-16 16:11:37 -0400

    added setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pcnc',
+      version='0.1',
+      description='Used in conjunction with MPB to design photonic crystal nanobeam cavities',
+      url='https://github.com/tsmahony/pcnc',
+      author='tsmahony',
+      author_email='tsmahony@gmail.com',
+      license='MIT',
+      packages=['pcnc'],
+      zip_safe=False)
